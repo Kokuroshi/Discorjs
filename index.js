e8f67d0c2a685359d5f5d72c711cb744141dc53a
Kokuroshi <77616026+Kokuroshi@users.noreply.github.com>
2021-02-17 19:54:41 +0100

    Pierwsze pliki przenoszenie

---
+++ index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+ 
+const client = new Discord.Client();
+ 
+ 
+ 
+client.once('ready', () => {
+    console.log('Bot jest online!');
+});
+ 
+ 
+ 
+client.login('ODExNjY0OTkyMTA5MzMwNDMz.YC1f7g.QV7ZsNKUDu5FfiL8D3ryarKbXBY');
